85206c7c9f26f62b196748ef0d1d4155d380744f
Jklebanovgithub <159466591+Jklebanovgithub@users.noreply.github.com>
2024-09-25 14:33:33 -0400

    done

--- src/App.java
+++ src/App.java
@@ -4,6 +4,31 @@ public class App extends PApplet{
     public static void main(String[] args)  {
         PApplet.main("App");
     }
+    public void settings(){
+        size(400,400);
+
+    }
+    public void setup(){
+        background(0,200,150);
+
+    }
+    public void draw(){
+        fill(0,0,200);
+        stroke(200,0,200);
+        strokeWeight(10);
+        rect(175,200,50,50);
+
+        
+        fill(200,0,0);
+        stroke(200,0,200);
+        strokeWeight(10);
+        ellipse(200,300,100,100);
+
+        stroke(0,0,0);
+        strokeWeight(5);
+        line(200,0,200,400);
+        
+    }
 
 
     
